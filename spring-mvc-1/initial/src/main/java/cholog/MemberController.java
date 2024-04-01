0e51107b7f0cdf8fd869c735a2232652521d0ee9
yireal <andytwo9878@gmail.com>
2024-04-01 21:42:50 +0900

    mvc-1/

--- spring-mvc-1/initial/src/main/java/cholog/MemberController.java
+++ spring-mvc-1/initial/src/main/java/cholog/MemberController.java
@@ -1,18 +1,24 @@
 package cholog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MemberController {
 
-    public String world() {
+    @GetMapping("/hello")
+    public String world(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name",name);
+        return "static";
         // TODO: /hello 요청 시 resources/templates/static.html 페이지가 응답할 수 있도록 설정하세요.
         // TODO: 쿼리 파라미터로 name 요청이 들어왔을 때 해당 값을 hello.html에서 사용할 수 있도록 하세요.
-        return null;
     }
 
-    public Person json() {
+    @GetMapping("/json")
+    public Person json(@RequestParam("name") String name,@RequestParam("age") Integer age) {
         // TODO: /json 요청 시 {"name": "brown", "age": 20} 데이터를 응답할 수 있도록 설정하세요.
-        return null;
+        return new Person(name,age);
     }
 }
